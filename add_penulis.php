eaab8a7fb37bda315570936eb547efcd28eedf78
syafiqa23 <111202314920@mhs.dinus.ac.id>
2025-01-12 19:16:02 +0700

    memodifikasi background add_penulis.php

--- add_penulis.php
+++ add_penulis.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <style>
         body {
             font-family: 'Arial', sans-serif;
-            background-color: #f8f9fa;
+            background-color: #e1f5fe;
             padding: 20px;
         }
         .container {
